2aa3c205c16e8034e031f41cc5db57a2079a4b72
x3bodee <a.m.basheer097@gmail.com>
2021-06-06 16:09:24 +0300

    edit specialty model

--- models/Specialty.model.js
+++ models/Specialty.model.js
@@ -2,22 +2,9 @@ const mongoose = require('mongoose');
 
 
 const specialtySchema = mongoose.Schema({
-    comment: {
-        type: String,
-        required: true
-    },
-    review_number: {
-        type: Integer,
-        required: true
-    },
-    patient: {
-        type : mongoose.Schema.Types.ObjectId , 
-        ref : 'User',
-        required: true
-    },
-    doctor: {
-        type : mongoose.Schema.Types.ObjectId , 
-        ref : 'User',
+    
+    name: {
+        type : String , 
         required: true
     },
     

d31fb1869a2aab37477f7c7baa7875cde304fc87
ammarbahasan1 <ak.bahassan@gmail.com>
2021-06-07 15:24:58 +0300

    show all Reviews

--- routes/review.route.js
+++ routes/review.route.js
@@ -29,4 +29,19 @@ router.post('/new', async (req, res) => {
 
 })
 
+
+// show all reviews
+router.get('/reviews', async (req, res) => {
+    try {
+        const allReviews = await Review.find()
+        res.status(200).json({ allReviews })
+    }
+    catch (err) {
+        res.status(401).json({
+            name: err.name,
+            message: err.message,
+            url: req.originalUrl
+        })
+    }
+})
 module.exports = router;

eb06b985674f99b7d51b276ce4943d922c0d5a83
raghadalawaji <alawaji.raghad@gmail.com>
2021-06-07 07:32:02 +0300

    Done login

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User.model')
 const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
 
 
@@ -24,9 +26,29 @@ router.post('/', async (req, res) => {
 
 })
 
-//login 
 
+// login
+router.post('/login' , async (req, res) => {
+    const {email , password } = req.body
+    try {
+        let user = await User.findOne({email: email})
+        if(user == null) throw new Error("this email is not in our db")
+        if(!bcrypt.compareSync(password , user.password)) throw new Error("password is wrong !! ")
+         user.password = undefined
+         let token = jwt.sign({user} ,
+            process.env.SECRETKEY , {
+             expiresIn : 60*60*1000
+         })
+        res.json({message : "login seccuss " , token})
+ 
+    } catch (err) {
+        res.status(401).json({name : err.name ,
+         message:err.message,
+        url : req.originalUrl
+        })
+    }
 
+})
 
 
 module.exports=router 

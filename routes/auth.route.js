7de00081bcd134a837dade457d887d6c430fd6b1
raghadalawaji <alawaji.raghad@gmail.com>
2021-06-07 14:47:46 +0300

    Done update/delete/logout

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,6 +4,7 @@ const User = require('../models/User.model')
 const bcrypt = require('bcryptjs');
 const jwt = require("jsonwebtoken");
 require('dotenv').config();
+const { ObjectId } = require('mongodb');
 
 
 
@@ -48,6 +49,65 @@ router.post('/login' , async (req, res) => {
         })
     }
 
+})
+// logout
+router.post('/logout' , async (req, res) => {
+    res.json({message : "logout seccuss " , token:""})
+
+})
+
+//update
+router.put('/update/:id' , async function(req, res){
+    var conditions = {
+        _id : ObjectId(req.params.id),
+        Fname:req.body.Fname,
+        Lname:req.body.Lname,
+        email:req.body.email,
+        password:req.body.password,
+        gender:req.body.gender,
+        location:req.body.location,
+        city:req.body.city
+    };
+    try{
+        let user = await User.findOne({_id: conditions._id});
+        user.Fname=conditions.Fname;
+        user.Lname=conditions.Lname;
+        user.email=conditions.email;
+        user.password=conditions.password;
+        user.gender=conditions.gender;
+        user.location=conditions.location;
+        user.city=conditions.city;
+        await user.save();
+        res.json({
+            message: "thank you for update"
+            , user: user
+            , success : true
+        })
+    }catch(err){
+        res.status(404).json({name : err.name ,
+            message:err.message,
+            url : req.originalUrl
+        })
+    }
+})
+
+//delete
+router.delete('/delete/:id' , async function(req, res){
+    var id = ObjectId(req.params.id);
+    try{
+        let user = await User.deleteOne({_id: id});
+
+        res.json({
+            message: "thank you for delete"
+            , user: user
+            , success : true
+        })
+    }catch(err){
+        res.status(404).json({name : err.name ,
+            message:err.message,
+            url : req.originalUrl
+        })
+    }
 })
 
 
